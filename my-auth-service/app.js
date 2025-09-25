93b7b868d8423efa2f037e48ce540055e3b7f810
Javier Prada <josejavierprada927@gmail.com>
2025-09-25 00:39:31 -0500

    Corrige configuración CORS y actualiza dependencias para despliegue

--- my-auth-service/app.js
+++ my-auth-service/app.js
@@ -11,9 +11,21 @@ const userRoutes = require('./routes/userRoutes');
 
 // Inicializa la aplicación Express
 const app = express();
-app.use(cors());
-// Middleware para parsear el cuerpo de las solicitudes en formato JSON
-// Express lee los datos enviados en las solicitudes POST (registro, login)
+
+const allowedOrigins = ['http://localhost:5173', 'https://tu-frontend-en-produccion.com']; // Agrega otros orígenes si es necesario
+app.use(cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('No permitido por CORS'));
+      }
+   },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+
+}));
+
 app.use(express.json());
 
 // Prueba la conexión a la base de datos al iniciar la aplicación
