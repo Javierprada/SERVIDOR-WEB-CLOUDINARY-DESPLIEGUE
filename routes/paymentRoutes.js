fdc4876e1c1ff0721761ea1369ea0a25a0918cfa
Javier Prada <josejavierprada927@gmail.com>
2025-09-24 21:25:32 -0500

    Versión Despliegue en Railway 24/09/2025

--- routes/paymentRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const processPayment  = require('../controllers/processPayment'); // Importa el controlador 
-
-const router = express.Router();
-
-// Define la ruta POST para procesar pagos
-router.post('/process-payment', processPayment);
-
-module.exports = router;
